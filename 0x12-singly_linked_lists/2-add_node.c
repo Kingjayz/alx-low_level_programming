022053d53d829b2c7bac726fe46264c030f2b90e
Kingjayz <tonimex2@gmail.com>
2022-11-24 05:24:54 -0800

    C - Singly linked lists commit

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning
+ * of a list_t list
+ * @head: pointer to head of the linked list
+ * @str: string to be added to list_t list
+ *
+ * Return: Function fails - NULL.
+ * Otherwise - address of the new element
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	size_t nchar;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+
+	for (nchar = 0; str[nchar]; nchar++)
+		;
+
+	new->len = nchar;
+	new->next = *head;
+	*head = new;
+
+	return (*head);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This deals with 0x12-singly_linked_lists. Linked lists are fundamental part of how modern day computers work.
